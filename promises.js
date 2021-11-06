8f03e8b920dc97fa68423ba2d7fb866be8bc281d
anonymousdomain <domainanonymous1@gmail.com>
2021-11-06 20:52:54 +0300

    using promise

---
+++ promises.js
@@ -0,0 +1,43 @@
+const students = [
+    {
+        firstname: 'dawit',
+        lastname: 'mekonnen',
+        desc: 'fucking loser '
+    },
+    {
+        firstname: 'nahom',
+        lastname: 'bezin',
+        desc: 'he sucks'
+    }
+]
+
+function getStudents() {
+    setTimeout(() => {
+        let list = ''
+        students.forEach((student) => {
+            list += `<li>${student.desc}</li>`;
+        });
+        document.body.innerHTML = list;
+    }, 1000);
+}
+
+function addStudent(student) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            students.push(student);
+            err = false;
+            if (!err) {
+                resolve();
+            } else {
+                reject('err:somthing went wrong')
+            }
+        }, 2000)
+    })
+
+}
+
+addStudent({
+    firstname:'eyuel',
+    lastname:'sahilu',
+    desc:'alcholic'
+}).then(getStudents);
